ea47e8ac66acbae49134f3a3f0c651e6c5ea7801
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-07-19 19:40:25 +0530

    Setup a "hover" effect to change grid colors
    This leaves a pixelated trail akin to something like a pen would when
    the mouse enters the container divs.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const innerContainer = document.createElement("div");
     innerContainer.setAttribute("class", "inner-container");
+
+    innerContainer.addEventListener("mouseenter", () => {
+      innerContainer.style.backgroundColor = "black";
+    });
+
     outerContainer.appendChild(innerContainer);
   }
   mainContainer.appendChild(outerContainer);

d999e30ab8bcb38ccf79df6d830bb49e9330fd02
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-07-19 19:05:59 +0530

    Apply logic to dynamically create 16x16 divs
    The basic idea is to use 16 outer containers, each of which will house
    16 inner containers; therefore, getting a total of 16x16 square divs
    inside the main container.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Etch-A-Sketch");
+const mainContainer = document.querySelector(".main-container");
+
+for (let i = 0; i < 16; i++) {
+  const outerContainer = document.createElement("div");
+  outerContainer.setAttribute("class", "outer-container");
+  for (let j = 0; j < 16; j++) {
+    const innerContainer = document.createElement("div");
+    innerContainer.setAttribute("class", "inner-container");
+    outerContainer.appendChild(innerContainer);
+  }
+  mainContainer.appendChild(outerContainer);
+}

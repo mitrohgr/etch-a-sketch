364158c24c0b1db03ec87aad4854f3b500173861
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-07-20 23:21:24 +0530

    Prompt the user for the number of square divs

--- js/script.js
+++ js/script.js
@@ -15,3 +15,19 @@ for (let i = 0; i < 16; i++) {
   }
   mainContainer.appendChild(outerContainer);
 }
+
+const modifyButton = document.querySelector(".modify-button");
+
+const DEFAULT_GRID_SIZE = 16;
+let gridSize = DEFAULT_GRID_SIZE;
+
+modifyButton.addEventListener("click", () => {
+  while (true) {
+    gridSize = +prompt("How many squares per side do you want?", 16);
+    if (gridSize >= 16 && gridSize <= 99) {
+      break;
+    } else {
+      alert("You can only use values from 16 to 99!");
+    }
+  }
+});

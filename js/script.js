db12bbfa8b5531313ac7798fa12c1634598d19ba
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-07-22 00:28:46 +0530

    Randomize the square divs' RGB values when checked
    With each interaction the RGB values will be randomly selected using
    getRandomIntInclusive(min, max) where min can be 0 and max can be 255.

--- js/script.js
+++ js/script.js
@@ -17,21 +17,47 @@ modifyButton.addEventListener("click", () => {
   createGrid(gridSize);
 });
 
+const randomizeBox = document.querySelector("input[name=randomize]");
+
+let randomized = false;
+randomizeBox.addEventListener("change", function (e) {
+  if (e.target.checked) {
+    randomized = true;
+  } else {
+    randomized = false;
+  }
+});
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function createGrid(gridSize) {
   mainContainer.replaceChildren();
+  
   for (let i = 0; i < gridSize; i++) {
     const outerContainer = document.createElement("div");
     outerContainer.setAttribute("class", "outer-container");
+    
     for (let j = 0; j < gridSize; j++) {
       const innerContainer = document.createElement("div");
       innerContainer.setAttribute("class", "inner-container");
-      innerContainer.addEventListener("mouseenter", () => {
-        innerContainer.style.backgroundColor = "black";
+      innerContainer.addEventListener("mouseenter", function (e) {
+        
+        if (randomized) {
+          innerContainer.style.backgroundColor = `rgb(${getRandomIntInclusive(0, 255)},
+          ${getRandomIntInclusive(0, 255)}, ${getRandomIntInclusive(0, 255)})`;
+        } else {
+          innerContainer.style.backgroundColor = "black";
+        }
       });
+      
       outerContainer.appendChild(innerContainer);
     }
     mainContainer.appendChild(outerContainer);
-  }  
+  }
 }
 
 createGrid(gridSize);
